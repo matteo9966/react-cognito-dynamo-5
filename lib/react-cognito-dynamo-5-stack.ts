b85d69ab91cae019fc2be90c850f5e098615e571
Matteo Luigi Tommasi <matteoluigitommasi@gmail.com>
2026-08-13 17:43:18 +0200

    added routing to the react application

--- lib/react-cognito-dynamo-5-stack.ts
+++ lib/react-cognito-dynamo-5-stack.ts
@@ -1,21 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { S3CloudFrontStaticWebsite } from './s3-cloudfront-static-website';
+// import * as cdk from 'aws-cdk-lib';
+// import { Construct } from 'constructs';
+// import { S3CloudFrontStaticWebsite } from './s3-cloudfront-static-website';
 
-export class ReactCognitoDynamo5Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+// export class ReactCognitoDynamo5Stack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
 
-    const frontendWebsite = new S3CloudFrontStaticWebsite(this, 'FrontendWebsite');
+//     const frontendWebsite = new S3CloudFrontStaticWebsite(this, 'FrontendWebsite');
 
-    new cdk.CfnOutput(this, 'FrontendBucketName', {
-      value: frontendWebsite.bucket.bucketName,
-      description: 'S3 bucket for the frontend dist files',
-    });
+//     new cdk.CfnOutput(this, 'FrontendBucketName', {
+//       value: frontendWebsite.bucket.bucketName,
+//       description: 'S3 bucket for the frontend dist files',
+//     });
 
-    new cdk.CfnOutput(this, 'FrontendUrl', {
-      value: `https://${frontendWebsite.distribution.distributionDomainName}`,
-      description: 'CloudFront URL for the frontend',
-    });
-  }
-}
+//     new cdk.CfnOutput(this, 'FrontendUrl', {
+//       value: `https://${frontendWebsite.distribution.distributionDomainName}`,
+//       description: 'CloudFront URL for the frontend',
+//     });
+//   }
+// }
